010f91ca307354037421df18071664dfe8f7401c
Ameer Deen <writeameer@gmail.com>
2023-01-10 22:35:59 +0400

    Removing data.json

---
+++ ArkServer.Entities/publish.ps1
@@ -0,0 +1,2 @@
+$packageName = (gci .\bin\release\ArkServer.Entities.0.0.1.nupkg | select FullName).FullName
+dotnet nuget push "$packageName" --api-key "$env:NUGET_API_KEY" --source https://api.nuget.org/v3/index.json

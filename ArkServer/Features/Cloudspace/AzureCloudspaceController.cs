fc8d383672f9b723f88190a67e17195093db112b
Ameer Deen <writeameer@gmail.com>
2023-01-17 19:42:20 +0400

    Cleaned up Ark space deletion

--- ArkServer/Features/Cloudspace/AzureCloudspaceController.cs
+++ ArkServer/Features/Cloudspace/AzureCloudspaceController.cs
@@ -3,107 +3,99 @@ using ArkServer.Repositories;
 using ArkServer.Services;
 using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Net;
 
-namespace ArkServer.Features.Cloudspace
+namespace ArkServer.Features.Cloudspace;
+
+[ApiController]
+public class AzureCloudspaceController : ControllerBase
 {
-    [ApiController]
-    public class AzureCloudspaceController : ControllerBase
+    private readonly AsbService _asbService;
+    private readonly Ark Ark;
+    private ILogger<AzureCloudspaceController> _logger;
+    private string ApiHost => HttpContext.Request.Host.ToString();
+    private IArkRepo _arkRepo;
+
+    public AzureCloudspaceController(ILogger<AzureCloudspaceController> logger,AsbService asbService,IArkRepo arkRepo)
     {
-        private readonly AsbService _asbService;
-        private readonly Ark _ark;
-        private ILogger<AzureCloudspaceController> _logger;
-        private string ApiHost => HttpContext.Request.Host.ToString();
-        private IArkRepo _arkRepo;
+        _asbService = asbService;
+        _logger = logger;
+        _arkRepo = arkRepo;
+        Ark= arkRepo.Get();
+    }
 
-        public AzureCloudspaceController(ILogger<AzureCloudspaceController> logger,AsbService asbService,IArkRepo arkRepo)
+    [HttpPost]
+    [Route("/azure/cloudspace")]
+    public async Task<IResult> Post(CreateAzureCloudspaceRequest req)
+    {
+        var exists = true;
+        var uri = $"https://{ApiHost}/azure/cloudspace/{req.Name}";
+        
+        // Ensure cloudspace name is 'default' only
+        if (req.Name.ToLower() !=  "default")
         {
-            _asbService = asbService;
-            _logger = logger;
-            _arkRepo = arkRepo;
-            _ark= arkRepo.Get();
+            return Results.Problem($"Cloudspace name was {req.Name}. Only 'default' is accepted as a cloudspace name");
+        }
+        
+        // If Cloudspace doesn't exist, create it.
+        if (Ark.AzureCloudspaces.Count == 0)
+        {
+            Ark.AzureCloudspaces = new List<AzureCloudspace> { new AzureCloudspace()};
+            exists= false;
+        } 
+ 
+        // Get Azure Cloudspace from Ark
+        var acs = Ark.AzureCloudspaces[0];
+
+        // Add Environments to cloudspace and Save to DB
+        req.Environments.ToList().ForEach(x => acs.AddSpoke(x));
+        _arkRepo.Save(Ark);
+
+        // Send Message to queue
+        var subject = req.GetType().Name;
+        await _asbService.Sender.SendMessageAsync(new ServiceBusMessage(acs.ToString()) { Subject = subject });
+
+        // Return status
+        if (exists)
+        {
+            // Send message to worker to run the pulumi handler program but return a 409 (Conflict) with Location header to resource
+            HttpContext.Response.Headers.Location = uri;
+            return Results.Conflict();
         }
 
-        [HttpPost]
-        [Route("/azure/cloudspace")]
-        public async Task<IResult> Post(CreateAzureCloudspaceRequest req)
+        // Send message to worker to run the pulumi handler program & return a 202 (Accepted)
+        return Results.Accepted(uri);
+
+    }
+
+
+    [HttpDelete]
+    [Route("/azure/cloudspace")]
+    public async Task<IResult> Delete(DeleteAzureCloudspaceRequest req)
+    {
+        // Ensure cloudspace name is 'default' only
+        if (req.Name.ToLower() != "default")
         {
-            var exists = true;
-            var uri = $"https://{ApiHost}/azure/cloudspace/{req.Name}";
-            
-            
-            // If Cloudspace doesn't exist, create it.
-            if (_ark.AzureCloudspaces.Count == 0)
-            {
-                _ark.AzureCloudspaces = new List<AzureCloudspace> { new AzureCloudspace()};
-                exists= false;
-            } 
-     
-            // Get Azure Cloudspace from Ark
-            var acs = _ark.AzureCloudspaces[0];
+            return Results.Problem($"Cloudspace name was {req.Name}. Only 'default' is accepted as a cloudspace name");
+        }
+        var uri = $"https://{ApiHost}/azure/cloudspace/{req.Name}";
 
-            // Add Environments to cloudspace
-            foreach (var name in req.Environments)
-            {
-                acs.AddSpoke(name);
-            }
-
-            _arkRepo.Save(_ark);
-
-
-            // Send Message to queue
-            var subject = req.GetType().Name;
-            await _asbService.Sender.SendMessageAsync(new ServiceBusMessage(acs.ToString()) { Subject = subject });
-
-            // Return status
-            if (exists)
-            {
-                // Send message to worker to run the pulumi handler program but return a 409 (Conflict) with Location header to resource
-                HttpContext.Response.Headers.Location = uri;
-                return Results.Conflict();
-            }
-
-            // Send message to worker to run the pulumi handler program & return a 202 (Accepted)
-            return Results.Accepted(uri);
-
+        //If Cloudspace doesn't exist, return OK
+        if (Ark.AzureCloudspaces.Count == 0)
+        {
+            return Results.Ok("No cloudspace to delete");
         }
 
+        var acs = Ark.AzureCloudspaces[0];
 
-        [HttpDelete]
-        [Route("/azure/cloudspace")]
-        public async Task<IResult> Delete(DeleteAzureCloudspaceRequest req)
-        {
+        //// Send Message to queue
+        var subject = req.GetType().Name;
+        await _asbService.Sender.SendMessageAsync(new ServiceBusMessage(acs.ToString()) { Subject = subject });
 
-            if (req.Name.ToLower() !=  "default")
-            {
-                return Results.Problem($"Cloudspace name was {req.Name}. Only 'default' is accepted as a cloudspace name");
-            }
-            var uri = $"https://{ApiHost}/azure/cloudspace/{req.Name}";
+        // Remove cloudspace and save in DB
+        Ark.AzureCloudspaces.RemoveAll(x => x.Name == req.Name.ToLower());
+        _arkRepo.Save(Ark);
 
-            //var name = req.Name.ToLower();
-            //_ark.AzureCloudspaces[0].DelSpoke(name);
-
-            Console.WriteLine($"This is a DeleteAzureCloudspaceRequest for {req.Name}");
-            
-            return null;
-            //// Add the cloudspace to Ark DB
-            //var exists = !await _arkService.AddCloudSpace(cs);
-
-            //// Send Message to queue
-            //var subject = cs.GetType().Name;
-            //await _asbService.Sender.SendMessageAsync(new ServiceBusMessage(cs.ToString()) { Subject = subject });
-
-            //// Return status
-            //if (exists)
-            //{
-            //    // Send message to worker to run the pulumi handler program but return a 409 (Conflict) with Location header to resource
-            //    HttpContext.Response.Headers.Location = uri;
-            //    return Results.Conflict();
-            //}
-
-            //// Send message to worker to run the pulumi handler program & return a 202 (Accepted)
-            //return Results.Accepted(uri);
-        }
+        // Send message to worker to run the pulumi handler program & return a 202 (Accepted)
+        return Results.Accepted();
     }
 }

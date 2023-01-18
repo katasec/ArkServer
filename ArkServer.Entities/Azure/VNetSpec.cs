1fbdb2d0c57f59c74707efe254941e866520cb27
Ameer Deen <writeameer@gmail.com>
2023-01-18 17:43:05 +0400

    Updated Yaml Deserializer to ignore extra properties

---
+++ ArkServer.Entities/Azure/VNetSpec.cs
@@ -0,0 +1,20 @@
+﻿namespace ArkServer.Entities.Azure;
+
+public record VNetSpec( string Name,  string AddressPrefix, IEnumerable<SubnetInfo> SubnetsInfo)
+{
+    internal int SpokeOctet2Offset = 0;
+    public string Octet2{
+        get
+        {
+            return AddressPrefix.Split(".")[1];
+        }
+    }
+
+    public string Octet1{
+        get
+        {
+            return AddressPrefix.Split(".")[0];
+        }
+    }
+};
+

a1ce0ac651d06dc738281afec2b0c9e83f6fa6f4
Ameer Deen <writeameer@gmail.com>
2023-01-27 22:29:40 +0400

    Adding a test hello world handler

---
+++ ArkServer.Entities/Azure/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ArkServer.Entities.Azure;
+
+public class BaseEntity
+{
+
+    //[JsonPropertyName("Id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Entity creation  is a long running operation. Operation status is interrogated using an entity's UpdateId 
+    /// </summary>
+    //[JsonPropertyName("UpdateId")]
+    public string? UpdateId { get; set; }
+
+    public BaseEntity()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
+}
---
+++ ArkServer.Entities/Azure/HelloGood.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ArkServer.Entities.Azure
+{
+    public class HelloSuccess: BaseEntity
+    {
+        public string? Message { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions{WriteIndented = true});
+        }
+    }
+
+    public class HelloFail: BaseEntity
+    {
+        public string? Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions{WriteIndented = true});
+        }
+    }
+}

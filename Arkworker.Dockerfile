e87d2ee9dc1ec419e073b8c6a8f59d23268a1064
Ameer Deen <writeameer@gmail.com>
2023-06-02 17:51:03 +0400

    Building ark worker and server from .net repo and with same versions

--- Arkworker.Dockerfile
+++ Arkworker.Dockerfile
@@ -2,10 +2,13 @@
 FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
 WORKDIR /app
 COPY . .
-RUN dotnet publish ArkWorker/Ark.Worker.csproj -c release -o out
+RUN dotnet publish ArkWorker/Ark.Worker.csproj -c release -o out && \
+    wget https://get.pulumi.com/releases/sdk/pulumi-v3.64.0-linux-x64.tar.gz && \
+    tar -xzvf pulumi-v3.64.0-linux-x64.tar.gz --directory /tmp/ 
 
 # Stage 2: Copy the application to a new container
 FROM mcr.microsoft.com/dotnet/sdk:7.0
 WORKDIR /app
 COPY --from=build /app/out .
+COPY --from=build /tmp/pulumi/pulumi /usr/local/bin/pulumi
 ENTRYPOINT ["/app/Ark.Worker"]
--- build.ps1
+++ build.ps1
@@ -1,6 +1,51 @@
-$tag = git describe --tags --abbrev=0
-$image = "ghcr.io/katasec/arkserver:$tag"
-docker build . -t  $image
+#!/usr/bin/env pwsh
 
+param (
+    [Parameter(Mandatory)]
+    [string]$target 
+)
 
-#dotnet publish --os linux --arch x64 /t:PublishContainer -c Release
+# ------------------------------------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------------------------------------
+
+function Test-Usage {
+    if ($target -ne "worker" -and $target -ne "server") {
+        $message = "-target should be 'worker' or 'server', got $($target)"
+        throw $message
+    }
+}
+
+function Get-DockerFileName() {
+    if ($target -eq "worker") {
+        return "Arkworker.Dockerfile"
+    }
+    else {
+        return "ArkServer.Dockerfile"
+    }
+}
+
+function Get-DockerImageName() {
+    $tag = git describe --tags --abbrev=0
+    if ($target -eq "worker") {
+        return "ghcr.io/katasec/arkworker:$tag"
+    }
+    else {
+        return "ghcr.io/katasec/arkserver:$tag"
+    }
+}
+
+function Build-Image() {
+    $dockerFileName = Get-DockerFileName
+    $dockerImageName = Get-DockerImageName
+    Write-Output "Building $dockerImageName"
+    Write-Output "Using $dockerFileName"
+    docker build . -t $dockerImageName -f $dockerFileName
+}
+
+# ------------------------------------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------------------------------------
+
+Test-Usage
+Build-Image
